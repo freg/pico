b4e8986bd66d82f3e384c7d20e1bd678863e4ae6
freg <frregnier@gmail.com>
2024-10-16 23:52:45 +0200

    analyse sur les 2 canaux et cette fois avec un last ok

--- tourniquet.py
+++ tourniquet.py
@@ -12,31 +12,40 @@ import numpy as np
 
 pid = os.getpid()
 python_process = psutil.Process(pid)
-tourniquet = []
-taille = 1000
+tourniquetA = []
+tourniquetB = []
+taille = 4000
 nline = 0
-last = []
-def tourniquet_fenetre(tourniquet, nline, last):
+lastA = []
+lastB = []
+def tourniquet_fenetre(tourniquet, nline, last, canal):
     new = [np.min(tourniquet), np.max(tourniquet),
            np.average(tourniquet), 
            np.var(tourniquet), 
            np.std(tourniquet)]
+    #print ("new:", new, "last:", last)
     if len(last) and new != last:
         print("anomalie ligne %d : %r\n"%(nline, new))
     last = new
+    return last
         
     
-def tourniquet_lire(fi, tourniquet, nline, last):
+def tourniquet_lire(fi, tourniqueta, tourniquetb, nline, lasta, lastb):
     try:
         line = fi.readline()
         data = line.split(',')
         nline += 1
-        if len(tourniquet) < taille:
-            tourniquet.append(eval(data[1]))
+        if len(tourniqueta) < taille:
+            tourniqueta.append(eval(data[1]))
         else:
-            tourniquet_fenetre(tourniquet, nline, last)
-            tourniquet = tourniquet[1:]
-            tourniquet.append(eval(data[1]))
+            lasta = tourniquet_fenetre(tourniqueta, nline, lasta, "A")
+            tourniqueta = tourniqueta[1:]
+        if len(tourniquetb) < taille:
+            tourniquetb.append(eval(data[3]))
+        else:
+            lastb = tourniquet_fenetre(tourniquetb, nline, lastb, "B")
+            tourniquetb = tourniquetb[1:]
+            tourniquetb.append(eval(data[3]))
     except Exception as e:
         print(e)
         import pdb;pdb.set_trace()
@@ -48,8 +57,8 @@ for i in range(1,12):
     fi.readline()
 
 while fi:
-    if cpt%100000 == 0:
+    if cpt%10000 == 0:
         memoryUse = python_process.memory_info()[0]/2.**30  # memory use in GB...I think
         print('memory use:', memoryUse, ' nline:', nline) 
     cpt+=1
-    nline = tourniquet_lire(fi, tourniquet, nline, last)
+    nline = tourniquet_lire(fi, tourniquetA, tourniquetB, nline, lastA, lastB)
